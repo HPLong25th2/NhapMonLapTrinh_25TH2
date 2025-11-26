6038358899fe3d4d8712127d49a4cc8cd881a406
Admin <Admin@ADMIN-PC>
2025-11-26 10:42:57 +0700

    26/11 sáng

--- NhapMonLapTrinh/Buoi5/Buoi5.cpp
+++ NhapMonLapTrinh/Buoi5/Buoi5.cpp
@@ -106,7 +106,7 @@ int main()
 	else {
 		cout << "Hai so khac dau: " << endl;
 	}*/
-    //Viết chương trình giải và biện luận phương trình bậc nhất ax + b = 0?
+	//Viết chương trình giải và biện luận phương trình bậc nhất ax + b = 0?
 //float a, b;
 //cout << "Nhap a, b: ";
 //cin >> a >> b;
@@ -119,7 +119,7 @@ int main()
 //else {
 //	cout << "Phuong trinh co 1 nghiem: " << endl;
 //}
-    //4 Nhập vào tháng của 1 năm. Cho biết tháng thuộc quý mấy trong năm?
+	//4 Nhập vào tháng của 1 năm. Cho biết tháng thuộc quý mấy trong năm?
 //int thang;
 //cout << "Nhap thang: ";
 //cin >> thang;
@@ -135,20 +135,147 @@ int main()
 //else {
 //	cout << "Quy 4:" << endl;
 //}
-    //Viết chương trình nhập 3 cạnh của 1 tam giác, cho biết đó là tam giác gì?
-float a, b, c;
-cout << "Nhap a, b, c: ";
-cin >> a >> b >> c;
-if (a == b && b == c) {
-	cout << "tam giac deu: " << endl;
-}
-else if (a == b || b == c || c =a) {
-	cout << "tam giac can: " << endl;
-}
+	//5.Viết chương trình nhập 3 cạnh của 1 tam giác, cho biết đó là tam giác gì?
+//float a, b, c;
+//cout << "Nhap a, b, c: ";
+//cin >> a >> b >> c;
+//if (a + b > c || b + c > a || a + c > b) {
+//	cout << "La mot tam giac: " << endl;
+//}
+//else if (a == b && b == c) {
+//	cout << "La mot tam giac deu: " << endl;
+//}
+//else if (a == b || b == c || a == c) {
+//	cout << "La mot tam giac can: " << endl;
+//}
+//else if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
+//	cout << "La mot tam giac vuong: " << endl;
+//}
+	//6. Viết chương trình giải phương trình bậc 2?
+	/*float a, b, c;
+	cout << "Nhap a, b ,c: ";
+	cin >> a >> b >> c;
+	if (a == 0 && b == 0 && c == 0) {
+		cout << "Phuong trinh vo so nghiem: " << endl;
+	}
+	else if (a == 0 && b == 0 && c != 0) {
+		cout << "Phuong trinh vo nghiem: " << endl;
+	}
+	else if (a == 0 && b != 0) {
+		cout << "Phuong trinh co nghiem: " << -c / b << endl;
+	}
+	else {
+		float d = b * b - 4 * a * c;
+		if (d < 0) {
+			cout << "Phuong trinh vo nghiem: " << endl;
+		}
+		else if (d == 0) {
+			cout << "Phuong trinh co nghiem: " << -b / (2 * a) << endl;
+		}
+		else {
+			cout << "x1 = " << (-b - sqrt(d)) / (2 * a) << endl;
+			cout << "x2 = " << (-b + sqrt(d)) / (2 * a) << endl;
+		}
+	}*/
+	//7 .Viết chương trình nhập tháng, năm. Hãy cho biết tháng đó có bao nhiêu ngày?
+	/*int thang, nam;
+	cout << "Nhap thang: ";
+	cin >> thang;
+	if (thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12) {
+		cout << "Thang " << thang << " co 31 ngay" << endl;
+	}
+	else if (thang == 2) {
+		cout << "Nhap nam: ";
+		cin >> nam;
+		if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0)) {
+			cout << "Thang " << thang << " co 29 ngay" << endl;
+		}
+		else {
+			cout << "Thang " << thang << " co 28 ngay" << endl;
+		}
+	}
+	else if (thang == 4 || thang == 6 || thang == 9 || thang == 11) {
+		cout << "Thang " << thang << " co 30 ngay" << endl;
+	}
+	else {
+		cout << "1 nam chi co 12 thang, vui long nhap lai." << endl;
+	}*/
+    //8. Viết chương trình nhập vào 1 ngày ( ngày, tháng, năm). Tìm ngày kế ngày vừa nhập (ngày, tháng, năm)?
+//int ngay, thang, nam;
+//cout << "Nhap ngay, thang, nam: ";
+//cin >> ngay >> thang >> nam;
+//if (ngay == 31 && thang == 12) {
+//	cout << "Ngay tiep theo: 1/1" << nam + 1 << endl;
+//}
+//else {
+//	int ngaycuoithang;
+//	if (thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12)
+//		ngaycuoithang = 31;
+//	else if (thang == 4 || thang == 6 || thang == 9 || thang == 11)
+//		ngaycuoithang = 30;
+//	else {
+//		if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+//			ngaycuoithang = 29;
+//		else
+//			ngaycuoithang = 28;
+//	}
+//	if (ngay < ngaycuoithang)
+//		cout << "Ngay tiep theo la: " << ngay + 1 << "/" << thang << "/" << nam << endl;
+//	else
+//		cout << "Ngay tiep theo la: 1/" << thang + 1 << "/" << nam << endl;
+//}
+    //9. Viết chương trình nhập vào 1 ngày ( ngày, tháng, năm). Tìm ngày trước ngày vừa nhập (ngày, tháng, năm)?
+
+    int ngay, thang, nam;
+    cout << "Nhap ngay, thang, nam: ";
+    cin >> ngay >> thang >> nam;
+    if (ngay == 1 && thang == 1) {
+        cout << "Ngay truoc do: 31/12/" << nam - 1;
+    }
+    else {
+        int ngaytruoc;
+        int thangtruoc = thang;
+        int namtruoc = nam;
+        if (ngay > 1) {
+            cout << "Ngay truoc do la: " << ngay - 1 << "/" << thang << "/" << nam << endl;
+        }
+        else {
+            thangtruoc = thang - 1;
+            if (thangtruoc == 0) {
+                thangtruoc = 12;
+                namtruoc = nam - 1;
+            }
+            if (thangtruoc == 1 || thangtruoc == 3 || thangtruoc == 5 || thangtruoc == 7 || thangtruoc == 8 || thangtruoc == 10 || thangtruoc == 12)
+                ngaytruoc = 31;
+            else if (thangtruoc == 4 || thangtruoc == 6 || thangtruoc == 9 || thangtruoc == 11)
+                ngaytruoc = 30;
+            else {
+                if ((namtruoc % 4 == 0 && namtruoc % 100 != 0) || (namtruoc % 400 == 0))
+                    ngaytruoc = 29;
+                else
+                    ngaytruoc = 28;
+            }
+            cout << "Ngay truoc do la: " << ngaytruoc << "/" << thangtruoc << "/" << namtruoc << endl;
+        }
+    }
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
