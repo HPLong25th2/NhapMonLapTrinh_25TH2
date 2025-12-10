f7a1673ca521866c98636e8284563b83473a169b
Admin <Admin@ADMIN-PC>
2025-12-10 13:41:52 +0700

    baii tap buoi 12

--- NhapMonLapTrinh/Buoi6/Buổi 12/Buổi 12/Baitap.h
+++ NhapMonLapTrinh/Buoi6/Buổi 12/Buổi 12/Baitap.h
@@ -1,1 +1,6 @@
 void nhap(int arr[], int& n);
+void xuat(int arr[], int n);
+void sochan(int arr[], int n);
+void vitriam(int arr[], int n);
+void giatrilonnhat(int arr[], int n);
+void vitrinhonhat(int arr[], int n);
--- NhapMonLapTrinh/Buoi6/Buổi 12/Buổi 12/Buoi12.cpp
+++ NhapMonLapTrinh/Buoi6/Buổi 12/Buổi 12/Buoi12.cpp
@@ -7,7 +7,13 @@ int main()
     const int SIZE = 100;
     int arr[SIZE], n = 0;
     nhap(arr, n);
-  
+    //xuat(arr, n);
+    //sochan(arr, n);
+   //vitriam(arr, n);
+    //giatrilonnhat(arr, n);
+    //vitrinhonhat(arr, n);
+    
+
 }
     
 
